64891d20ea80052504a5dc6684638ef1e9d5a937
suveng <suveng@163.com>
2018-10-13 23:36:31 +0800

    增加todo list

--- src/main/java/my/suveng/veng_bike_server/vehicle/controller/BikeController.java
+++ src/main/java/my/suveng/veng_bike_server/vehicle/controller/BikeController.java
@@ -2,6 +2,7 @@ package my.suveng.veng_bike_server.vehicle.controller;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import my.suveng.veng_bike_server.user.pojo.mongo.User;
 import my.suveng.veng_bike_server.vehicle.pojo.mongo.Bike;
 import my.suveng.veng_bike_server.vehicle.service.BikeService;
 import org.springframework.data.geo.GeoResults;
@@ -19,7 +20,7 @@ import java.util.List;
  * email  1344114844@qq.com
  * date   18-8-23 下午5:54
  */
-@Api(value="bike", tags={"bike接口模块"},description = "动车相关")
+@Api(value = "bike", tags = {"bike接口模块"}, description = "动车相关")
 @Controller
 public class BikeController {
     @Resource
@@ -44,10 +45,11 @@ public class BikeController {
     @ResponseBody
     @ApiOperation(value = "查找全部单车")
     public List<Bike> bikes() {
-        List<Bike> list=bikeService.findAll();
+        List<Bike> list = bikeService.findAll();
         System.out.println(list);
         return list;
     }
+
     @GetMapping("/bikes/near")
     @ResponseBody
     @ApiOperation(value = "查找附近单车")
@@ -58,11 +60,26 @@ public class BikeController {
 
     @GetMapping("/bike_list")
     @ApiOperation(value = "返回单车列表视图")
-    public String to_bike_list(){
+    public String to_bike_list() {
         return "bike/list";
     }
 
+    //TODO::扫码开锁功能：根据传入的 用户id 和 车辆编号 修改车辆状态 创建订单
+    @PostMapping("/vehicle/unlock")
+    @ResponseBody
+    @ApiOperation(value = "解锁车辆")
+    public String unlock(@RequestBody User user, @RequestBody Bike bike) {
 
+        return null;
+    }
 
+    //TODO：还车业务：上锁功能 和 计费功能
+    @PostMapping("/vehicle/lock")
+    @ResponseBody
+    @ApiOperation(value = "还车业务接口")
+    public String lock(@RequestBody User user, @RequestBody Bike bike) {
+
+        return null;
+    }
 
 }
