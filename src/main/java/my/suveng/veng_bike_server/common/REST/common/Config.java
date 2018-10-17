c4be3455e2222901a81718a7c00b622b4aa4989f
suveng <suveng@163.com>
2018-10-17 12:14:22 +0800

    还车部分业务
    Signed-off-by: suveng <suveng@163.com>

--- src/main/java/my/suveng/veng_bike_server/common/REST/common/Config.java
+++ src/main/java/my/suveng/veng_bike_server/common/REST/common/Config.java
@@ -13,12 +13,12 @@ public class Config
 	/**
 	 * 开发者注册后系统自动生成的账号，可在官网登录后查看
 	 */
-	public static final String ACCOUNT_SID = "f15cd7df99074b1f92752308b91635fe";
+	public static final String ACCOUNT_SID = "f15cd7df99074b1f92752308b91635fesu";
 
 	/**
 	 * 开发者注册后系统自动生成的TOKEN，可在官网登录后查看
 	 */
-	public static final String AUTH_TOKEN = "896073eb64fa457fac89e18cb235bfc1";
+	public static final String AUTH_TOKEN = "896073eb64fa457fac89e18cb235bfc1su";
 
 	/**
 	 * 响应数据类型, JSON或XML
