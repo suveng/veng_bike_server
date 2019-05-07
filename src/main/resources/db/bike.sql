fbd721c5107118b0cc3c85e4bdfb12cf292f994b
suveng <suveng@163.com>
2019-05-07 10:41:44 +0800

    更新表结构

--- src/main/resources/db/bike.sql
+++ src/main/resources/db/bike.sql
@@ -1,89 +1,93 @@
 /*
-Navicat MySQL Data Transfer
+ Navicat Premium Data Transfer
 
-Source Server         : localhost
-Source Server Version : 50725
-Source Host           : localhost:3306
-Source Database       : bike
+ Source Server         : local
+ Source Server Type    : MySQL
+ Source Server Version : 50703
+ Source Host           : localhost:3306
+ Source Schema         : bike
 
-Target Server Type    : MYSQL
-Target Server Version : 50725
-File Encoding         : 65001
+ Target Server Type    : MySQL
+ Target Server Version : 50703
+ File Encoding         : 65001
 
-Date: 2019-04-08 00:09:28
+ Date: 07/05/2019 10:39:38
 */
 
-SET FOREIGN_KEY_CHECKS=0;
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
 -- Table structure for rechargerecord
 -- ----------------------------
 DROP TABLE IF EXISTS `rechargerecord`;
-CREATE TABLE `rechargerecord` (
+CREATE TABLE `rechargerecord`  (
   `rechareId` bigint(20) NOT NULL AUTO_INCREMENT,
-  `userId` varchar(255) NOT NULL DEFAULT '',
-  `charge` double(255,0) NOT NULL DEFAULT '0',
+  `userId` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '',
+  `charge` double(255, 0) NOT NULL DEFAULT 0,
   `createTime` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
-  `province` varchar(255) NOT NULL DEFAULT '',
-  `city` varchar(255) NOT NULL DEFAULT '',
-  `district` varchar(255) NOT NULL DEFAULT '',
-  PRIMARY KEY (`rechareId`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+  `province` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '',
+  `city` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '',
+  `district` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '',
+  PRIMARY KEY (`rechareId`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
 
 -- ----------------------------
 -- Table structure for rentalpoint
 -- ----------------------------
 DROP TABLE IF EXISTS `rentalpoint`;
-CREATE TABLE `rentalpoint` (
-  `pointId` varchar(255) NOT NULL,
-  `longitude` double(255,6) NOT NULL DEFAULT '0.000000',
-  `latitude` double(255,6) NOT NULL DEFAULT '0.000000',
-  `leftBike` int(255) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`pointId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `rentalpoint`  (
+  `pointId` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `longitude` double(255, 6) NOT NULL DEFAULT 0.000000,
+  `latitude` double(255, 6) NOT NULL DEFAULT 0.000000,
+  `leftBike` int(255) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`pointId`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
 
 -- ----------------------------
 -- Table structure for rentalrecord
 -- ----------------------------
 DROP TABLE IF EXISTS `rentalrecord`;
-CREATE TABLE `rentalrecord` (
+CREATE TABLE `rentalrecord`  (
   `rentalId` bigint(20) NOT NULL AUTO_INCREMENT,
-  `userId` varchar(255) NOT NULL DEFAULT '',
-  `vehicleId` varchar(255) NOT NULL DEFAULT '',
-  `isFinish` int(255) NOT NULL DEFAULT '0',
-  `beginPoint` varchar(255) NOT NULL DEFAULT '',
-  `endPoint` varchar(255) NOT NULL DEFAULT '',
-  `beginTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `endTime` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`rentalId`)
-) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8;
+  `userId` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '',
+  `vehicleId` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '',
+  `isFinish` int(255) NOT NULL DEFAULT 0,
+  `beginPoint` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '',
+  `endPoint` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '',
+  `beginTime` timestamp(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+  `endTime` timestamp(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`rentalId`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 23 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
 
 -- ----------------------------
 -- Table structure for user
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
-CREATE TABLE `user` (
-  `userId` varchar(128) NOT NULL,
-  `status` int(2) NOT NULL DEFAULT '0',
-  `phoneNum` varchar(15) NOT NULL DEFAULT '0',
-  `name` varchar(12) NOT NULL DEFAULT '',
-  `idNum` varchar(25) NOT NULL DEFAULT '',
-  `deposit` double(255,0) NOT NULL DEFAULT '0',
-  `balance` double(255,0) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`userId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `user`  (
+  `userId` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `status` int(2) NOT NULL DEFAULT 0,
+  `phoneNum` varchar(15) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '0',
+  `name` varchar(12) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '',
+  `idNum` varchar(25) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '',
+  `deposit` double(255, 0) NOT NULL DEFAULT 0,
+  `balance` double(255, 0) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`userId`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
 
 -- ----------------------------
 -- Table structure for vehicle
 -- ----------------------------
 DROP TABLE IF EXISTS `vehicle`;
-CREATE TABLE `vehicle` (
-  `vehicleId` varchar(255) NOT NULL,
-  `qrCode` varchar(255) NOT NULL DEFAULT '',
-  `longitude` double(255,6) NOT NULL DEFAULT '0.000000',
-  `latitude` double(255,6) NOT NULL DEFAULT '0.000000',
-  `status` int(255) NOT NULL DEFAULT '0',
-  `type` int(255) NOT NULL DEFAULT '0',
-  `pointId` varchar(255) NOT NULL DEFAULT '',
-  PRIMARY KEY (`vehicleId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `vehicle`  (
+  `vehicleId` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `qrCode` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '',
+  `longitude` double(255, 6) NOT NULL DEFAULT 0.000000,
+  `latitude` double(255, 6) NOT NULL DEFAULT 0.000000,
+  `status` int(255) NOT NULL DEFAULT 0,
+  `type` int(255) NOT NULL DEFAULT 0,
+  `pointId` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '',
+  PRIMARY KEY (`vehicleId`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+
+SET FOREIGN_KEY_CHECKS = 1;
